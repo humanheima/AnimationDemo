ba521eafe1b5f496cebe289176c200157b9d1fa1
dumingwei <dumingwei07050@hellobike.com>
2019-08-19 13:43:59 +0800

    migate to AndroidX

--- app/src/main/java/com/hm/animationdemo/activity/LayoutAnimationActivity.java
+++ app/src/main/java/com/hm/animationdemo/activity/LayoutAnimationActivity.java
@@ -3,7 +3,7 @@ package com.hm.animationdemo.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
