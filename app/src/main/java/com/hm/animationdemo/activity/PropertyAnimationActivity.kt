c95af8f7ed1ed8a45b2c83d0068354e7ffddcb11
p_dmweidu <p_dmweidu@tencent.com>
2023-06-29 20:34:38 +0800

    一些修改。

--- app/src/main/java/com/hm/animationdemo/activity/PropertyAnimationActivity.kt
+++ app/src/main/java/com/hm/animationdemo/activity/PropertyAnimationActivity.kt
@@ -43,8 +43,19 @@ class PropertyAnimationActivity : AppCompatActivity() {
 
     fun onClick(view: View) {
         when (view.id) {
+            R.id.btnStartRotationAnimation1 -> {
+                val rotationYAnimator = ObjectAnimator.ofFloat(ivRotationY, "rotation", 0f, 180f)
+                rotationYAnimator.duration = 500
+                rotationYAnimator.start()
+            }
+            R.id.btnStartRotationAnimation2 -> {
+                val rotationYAnimator = ObjectAnimator.ofFloat(ivRotationY, "rotation", 180f, 0f)
+                rotationYAnimator.duration = 500
+                rotationYAnimator.start()
+            }
             R.id.btnTransAnimator -> {
-                val infiniteAnimator = ObjectAnimator.ofFloat(ll_translate_layout, "translationY", 0f, 100f)
+                val infiniteAnimator =
+                    ObjectAnimator.ofFloat(ll_translate_layout, "translationY", 0f, 100f)
                 infiniteAnimator.duration = 3000
                 infiniteAnimator.addListener(object : AnimatorListenerAdapter() {
                     override fun onAnimationEnd(animation: Animator?) {
@@ -55,7 +66,8 @@ class PropertyAnimationActivity : AppCompatActivity() {
             }
             R.id.btnRedPacketAnimator -> {
                 //角度大于0，是顺时针旋转
-                val rotateObjectAnimator = ObjectAnimator.ofFloat(ivRedPacket, "rotation", 0f, 10f, -10f, 10f, -10f, 10f)
+                val rotateObjectAnimator =
+                    ObjectAnimator.ofFloat(ivRedPacket, "rotation", 0f, 10f, -10f, 10f, -10f, 10f)
                 //val rotateObjectAnimator = ObjectAnimator.ofFloat(ivRedPacket, "rotation", 0f, 360f)
                 rotateObjectAnimator.duration = 1000L
                 //rotateObjectAnimator.setRepeatCount(1);
@@ -109,7 +121,14 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 infiniteAnimator.start()
             }
             R.id.btnTranslateInfinite -> {
-                val mLeftAnimator = ObjectAnimator.ofFloat(btnTranslateInfinite, "translationX", 0f, -100f, 100f, 0f)
+                val mLeftAnimator = ObjectAnimator.ofFloat(
+                    btnTranslateInfinite,
+                    "translationX",
+                    0f,
+                    -100f,
+                    100f,
+                    0f
+                )
                 //val mRightAnimator = ObjectAnimator.ofFloat(btnTranslateInfinite, "translationX", -100f, 100f)
                 //val mBackAnimator = ObjectAnimator.ofFloat(btnTranslateInfinite, "translationX", 100f, 0f)
 
@@ -141,8 +160,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 //从keyFrame1过渡到keyFrame2会使用这个差值器
                 keyFrame2.interpolator = BounceInterpolator()
 
-                val valuesHolder = PropertyValuesHolder.ofKeyframe("rotation",
-                        keyFrame0, keyFrame1, keyFrame2)
+                val valuesHolder = PropertyValuesHolder.ofKeyframe(
+                    "rotation",
+                    keyFrame0, keyFrame1, keyFrame2
+                )
                 val animator = ObjectAnimator.ofPropertyValuesHolder(ivPhone, valuesHolder)
                 animator.duration = 3000
                 animator.start()
@@ -161,9 +182,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 val keyFrame9 = Keyframe.ofFloat(0.9f, -0f)
                 val keyFrame10 = Keyframe.ofFloat(1f, 0f)
 
-                val valuesHolder = PropertyValuesHolder.ofKeyframe("rotation",
-                        keyFrame0, keyFrame1, keyFrame2, keyFrame3, keyFrame4,
-                        keyFrame5, keyFrame6, keyFrame7, keyFrame8, keyFrame9, keyFrame10
+                val valuesHolder = PropertyValuesHolder.ofKeyframe(
+                    "rotation",
+                    keyFrame0, keyFrame1, keyFrame2, keyFrame3, keyFrame4,
+                    keyFrame5, keyFrame6, keyFrame7, keyFrame8, keyFrame9, keyFrame10
                 )
                 val animator = ObjectAnimator.ofPropertyValuesHolder(ivPhone, valuesHolder)
                 animator.duration = 2000
@@ -183,9 +205,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 val keyFrame10 = Keyframe.ofFloat(1f, 0f)
 
                 //改变旋转属性
-                val valuesHolder = PropertyValuesHolder.ofKeyframe("rotation",
-                        keyFrame0, keyFrame1, keyFrame2, keyFrame3, keyFrame4,
-                        keyFrame5, keyFrame6, keyFrame7, keyFrame8, keyFrame9, keyFrame10
+                val valuesHolder = PropertyValuesHolder.ofKeyframe(
+                    "rotation",
+                    keyFrame0, keyFrame1, keyFrame2, keyFrame3, keyFrame4,
+                    keyFrame5, keyFrame6, keyFrame7, keyFrame8, keyFrame9, keyFrame10
                 )
 
                 val scaleXFrame0 = Keyframe.ofFloat(0f, 1f)
@@ -193,25 +216,31 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 val scaleXFrame2 = Keyframe.ofFloat(0.9f, 1.1f)
                 val scaleXFrame3 = Keyframe.ofFloat(1f, 1f)
 
-                val scaleXHolder = PropertyValuesHolder.ofKeyframe("scaleX",
-                        scaleXFrame0,
-                        scaleXFrame1,
-                        scaleXFrame2,
-                        scaleXFrame3)
+                val scaleXHolder = PropertyValuesHolder.ofKeyframe(
+                    "scaleX",
+                    scaleXFrame0,
+                    scaleXFrame1,
+                    scaleXFrame2,
+                    scaleXFrame3
+                )
 
                 val scaleYFrame0 = Keyframe.ofFloat(0f, 1f)
                 val scaleYFrame1 = Keyframe.ofFloat(0.1f, 1.1f)
                 val scaleYFrame2 = Keyframe.ofFloat(0.9f, 1.1f)
                 val scaleYFrame3 = Keyframe.ofFloat(1f, 1f)
 
-                val scaleYHolder = PropertyValuesHolder.ofKeyframe("scaleY",
-                        scaleYFrame0,
-                        scaleYFrame1,
-                        scaleYFrame2,
-                        scaleYFrame3)
+                val scaleYHolder = PropertyValuesHolder.ofKeyframe(
+                    "scaleY",
+                    scaleYFrame0,
+                    scaleYFrame1,
+                    scaleYFrame2,
+                    scaleYFrame3
+                )
 
-                val animator = ObjectAnimator.ofPropertyValuesHolder(ivPhone,
-                        valuesHolder, scaleXHolder, scaleYHolder)
+                val animator = ObjectAnimator.ofPropertyValuesHolder(
+                    ivPhone,
+                    valuesHolder, scaleXHolder, scaleYHolder
+                )
                 animator.duration = 2000
                 animator.start()
             }
@@ -220,15 +249,25 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 val valuesHolder1 = PropertyValuesHolder.ofFloat("scaleX", 1f, 1.5f, 1f)
                 val valuesHolder2 = PropertyValuesHolder.ofFloat("rotation", 0f, 90f, 0f)
                 val valuesHolder3 = PropertyValuesHolder.ofFloat("alpha", 1.0f, 0.3f, 1f)
-                val valuesHolder = ObjectAnimator.ofPropertyValuesHolder(btn_property_value_holder, valuesHolder1, valuesHolder2, valuesHolder3)
+                val valuesHolder = ObjectAnimator.ofPropertyValuesHolder(
+                    btn_property_value_holder,
+                    valuesHolder1,
+                    valuesHolder2,
+                    valuesHolder3
+                )
                 valuesHolder.duration = 2000
                 valuesHolder.start()
             }
             R.id.btnPropertyValueHolderOfObject -> {
                 Log.d(TAG, "onClick: btnPropertyValueHolderOfObject")
-                val valuesHolder1 = PropertyValuesHolder.ofObject("charText",
-                        CharEvaluator(), 'A', 'Z')
-                val objectAnimator = ObjectAnimator.ofPropertyValuesHolder(btnPropertyValueHolderOfObject, valuesHolder1)
+                val valuesHolder1 = PropertyValuesHolder.ofObject(
+                    "charText",
+                    CharEvaluator(), 'A', 'Z'
+                )
+                val objectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+                    btnPropertyValueHolderOfObject,
+                    valuesHolder1
+                )
                 objectAnimator.duration = 2000
                 objectAnimator.start()
             }
@@ -239,7 +278,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 animatorSet.start()*/
 
                 //获取ValueAnimator
-                val valueAnimator = AnimatorInflater.loadAnimator(this, R.animator.value_animator_first) as ValueAnimator
+                val valueAnimator = AnimatorInflater.loadAnimator(
+                    this,
+                    R.animator.value_animator_first
+                ) as ValueAnimator
 
                 valueAnimator.addUpdateListener { animation ->
                     Log.d(TAG, "animatedValue:${animation.animatedValue}")
@@ -248,7 +290,8 @@ class PropertyAnimationActivity : AppCompatActivity() {
             }
 
             R.id.btn_bg_animation -> {
-                val colorAnimator = ObjectAnimator.ofInt(btn_bg_animation, "backgroundColor", -0x7f80, -0x7f7f01)
+                val colorAnimator =
+                    ObjectAnimator.ofInt(btn_bg_animation, "backgroundColor", -0x7f80, -0x7f7f01)
                 colorAnimator.duration = 5000
                 colorAnimator.setEvaluator(ArgbEvaluator())
                 colorAnimator.repeatMode = ValueAnimator.REVERSE
@@ -267,7 +310,8 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 set.start()
             }
             R.id.tv_listen_animation -> {
-                val objectAnimator = ObjectAnimator.ofFloat(tv_listen_animation, "scaleX", 1.0f, 2.0f)
+                val objectAnimator =
+                    ObjectAnimator.ofFloat(tv_listen_animation, "scaleX", 1.0f, 2.0f)
                 objectAnimator.duration = 3000
                 /*objectAnimator.addListener(new AnimatorListenerAdapter() {
                     @Override
@@ -304,7 +348,8 @@ class PropertyAnimationActivity : AppCompatActivity() {
                         val currentValue = animation.animatedValue as Int
                         Log.e(TAG, "onAnimationUpdate currentValue = $currentValue")
                         val fraction = animation.animatedFraction
-                        btn_value_animation.layoutParams.width = mEvaluator.evaluate(fraction, btn_value_animation.width, 800)
+                        btn_value_animation.layoutParams.width =
+                            mEvaluator.evaluate(fraction, btn_value_animation.width, 800)
                         btn_value_animation.requestLayout()
                     }
                 })
@@ -315,9 +360,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 Log.e(TAG, "btn_pro_animation")
 
                 val swingAnimation = SwingAnimation(
-                        0f, 30f, -30f,
-                        Animation.RELATIVE_TO_SELF, 0.5f,
-                        Animation.RELATIVE_TO_SELF, 0.0f)
+                    0f, 30f, -30f,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.0f
+                )
                 swingAnimation.duration = 800     //动画持续时间
                 swingAnimation.fillAfter = false  //是否保持动画结束画面
                 //swingAnimation.setRepeatCount(2);
@@ -326,8 +372,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
             }
             R.id.btn_rotate_animation -> {
                 //角度大于0，是顺时针旋转
-                val rotateObjectAnimator = ObjectAnimator.ofFloat(ivBell,
-                        "rotation", 0f, 40f, -40f, 40f, -40f, 40f, -40f, 40f, -40f, 0f)
+                val rotateObjectAnimator = ObjectAnimator.ofFloat(
+                    ivBell,
+                    "rotation", 0f, 40f, -40f, 40f, -40f, 40f, -40f, 40f, -40f, 0f
+                )
                 rotateObjectAnimator.duration = 1500L
                 //rotateObjectAnimator.setRepeatCount(1);
 
@@ -355,8 +403,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 rotateObjectAnimator.start()
             }
             R.id.btnChangeText -> {
-                val animChangeText = ValueAnimator.ofObject(CharEvaluator(),
-                        'A', 'Z')
+                val animChangeText = ValueAnimator.ofObject(
+                    CharEvaluator(),
+                    'A', 'Z'
+                )
                 animChangeText.addUpdateListener { animation ->
                     val text = animation.animatedValue as Char
                     btnChangeText.text = text.toString()
@@ -366,11 +416,18 @@ class PropertyAnimationActivity : AppCompatActivity() {
             }
             R.id.btnAnimatorSetBuilder -> {
 
-                val colorAnimator = ObjectAnimator.ofInt(btnAnimatorSetBuilder, "backgroundColor", -0x7f80, -0x7f7f01)
+                val colorAnimator = ObjectAnimator.ofInt(
+                    btnAnimatorSetBuilder,
+                    "backgroundColor",
+                    -0x7f80,
+                    -0x7f7f01
+                )
                 colorAnimator.setEvaluator(ArgbEvaluator())
 
-                val translateY = ObjectAnimator.ofFloat(btnAnimatorSetBuilder, "translationY", 0f, 400f, 0f)
-                val translateX = ObjectAnimator.ofFloat(btnAnimatorSetBuilder, "translationX", 0f, 400f, 0f)
+                val translateY =
+                    ObjectAnimator.ofFloat(btnAnimatorSetBuilder, "translationY", 0f, 400f, 0f)
+                val translateX =
+                    ObjectAnimator.ofFloat(btnAnimatorSetBuilder, "translationX", 0f, 400f, 0f)
                 translateY.repeatCount = ValueAnimator.INFINITE
 
                 animatorSet = AnimatorSet()
@@ -408,11 +465,15 @@ class PropertyAnimationActivity : AppCompatActivity() {
 
             R.id.btnAnimatorSetDelay -> {
 
-                val tvTranslateY = ObjectAnimator.ofFloat(btnAnimatorSetDelay,
-                        "translationY", 0f, 50f, 0f)
+                val tvTranslateY = ObjectAnimator.ofFloat(
+                    btnAnimatorSetDelay,
+                    "translationY", 0f, 50f, 0f
+                )
 
-                val tvTranslateX = ObjectAnimator.ofFloat(btnCancelAnimatorSetBuilder,
-                        "translationX", 0f, 50f, 0f)
+                val tvTranslateX = ObjectAnimator.ofFloat(
+                    btnCancelAnimatorSetBuilder,
+                    "translationX", 0f, 50f, 0f
+                )
 
                 tvTranslateY.startDelay = 2000
                 tvTranslateX.startDelay = 2000
